4ab81e1f996283ae85936134ec2493af012f61d4
Wes Simcox <wes.simcox11@gmail.com>
2023-08-08 13:49:26 -0600

    adding code 2

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -1,11 +1,42 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+let deferredPrompt;
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+// Add an event handler to the `beforeinstallprompt` event
+window.addEventListener('beforeinstallprompt', (event) => {
+  event.preventDefault(); // Prevents the default behavior of the browser
+  deferredPrompt = event; // Store the event for later use
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+  // Show a button or some UI element to trigger the installation
+  butInstall.style.display = 'block';
+});
+
+// Implement a click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+  if (deferredPrompt) {
+    // Show the installation prompt
+    deferredPrompt.prompt();
+
+    // Wait for the user to respond to the prompt
+    const result = await deferredPrompt.userChoice;
+
+    // Check the user's choice and log the result
+    if (result.outcome === 'accepted') {
+      console.log('User accepted the installation prompt');
+    } else {
+      console.log('User dismissed the installation prompt');
+    }
+
+    // Clear the deferred prompt
+    deferredPrompt = null;
+
+    // Hide the install button
+    butInstall.style.display = 'none';
+  }
+});
+
+// Add a handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+  console.log('App was successfully installed');
+});
--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -3,27 +3,56 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './src/index.html', // Specify your HTML template path
+      }),
+      new WebpackPwaManifest({
+        name: 'My PWA',
+        short_name: 'PWA',
+        start_url: '/',
+        display: 'standalone',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        icons: [
+          {
+            src: path.resolve('src/assets/icon.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+          },
+        ],
+      }),
+      new InjectManifest({
+        swSrc: './src/sw.js', // Specify the path to your service worker script
+        exclude: [/\.map$/, /_redirects$/], // Exclude specific files from being precached
+      }),
     ],
-
     module: {
       rules: [
-        
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'], // Add CSS loaders
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader', // Add Babel loader
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
       ],
     },
   };
